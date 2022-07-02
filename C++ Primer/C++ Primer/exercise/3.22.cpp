1b448cbbd5223fbb411f12b9c40d0b75cad4abb4
云明正 <1374730170@qq.com>
2022-07-02 02:13:54 +0100

    cpp files

---
+++ C++ Primer/C++ Primer/exercise/3.22.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+//int main() {
+//	vector<string> text;
+//	string s = "my name is aaa";
+//	text.push_back(s);
+//	//cout << "please input a string";
+//	//while (getline(cin, s)) {
+//	//	text.push_back(s);
+//	//	cout << "please input a string";
+//	//}
+//
+//
+//	for (auto it = text.begin(); it != text.end() && !it->empty(); ++it) {
+//		for (auto it2 = it->begin(); it2 != it->end(); ++it2)
+//		{
+//			*it2 = toupper(*it2);
+//		}
+//		cout << *it << endl;
+//	}
+//	return 0;
+//}
---
+++ C++ Primer/C++ Primer/exercise/3.23.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+	vector<int> list;
+	for (int i = 1; i <= 10; i++)
+	{
+		list.push_back(i);
+	}
+
+	for (auto i = list.begin(); i != list.end(); ++i)
+	{
+		*i = (*i) * 2;
+		cout << *i << " ";
+	}
+	cout << endl;
+}
