61e04c60bdb1bc2e4de2e009de1f185034edf6cc
云明正 <1374730170@qq.com>
2022-07-05 00:45:07 +0100

    Singleton

---
+++ C++ Primer/C++ Primer/concept/Class/Singleton.cpp
@@ -0,0 +1,16 @@
+//
+//class Singleton
+//{
+//private:
+//	static Singleton* s_Instance;
+//public:
+//	static Singleton& Get() { return *s_Instance; }
+//
+//	void Call(){}
+//};
+//
+//Singleton* Singleton::s_Instance = nullptr;
+//
+//int main() {
+//	Singleton::Get().Call();
+//}
