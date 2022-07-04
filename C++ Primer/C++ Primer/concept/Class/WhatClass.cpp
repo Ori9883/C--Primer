cc8362c905f0516d00b1e8e585039acb237b35f9
云明正 <1374730170@qq.com>
2022-07-04 04:30:02 +0100

    class

---
+++ C++ Primer/C++ Primer/concept/Class/WhatClass.cpp
@@ -0,0 +1,36 @@
+//#include <iostream>
+//#include <string>
+//#include <vector>
+//using std::cin; using std::cout; using std::endl; using std::string; using std::vector;
+//using std::begin; using std::end;
+//
+//class Log
+//{
+//public:
+//	const int LogLevelError = 0;
+//	const int LogLevelWarning = 1;
+//	const int LogLevelInfo = 2;
+//private:
+//	int m_LogLevel;
+//public:
+//	void SetLevel(int level) {
+//		m_LogLevel = level;
+//	}
+//
+//	void Warn(const char* message) {
+//		if (m_LogLevel <= LogLevelWarning)
+//			std::cout << "[Warning]" << message << endl;
+//	}
+//
+//private:
+//
+//};
+//
+//int main() {
+//	Log log;
+//	log.SetLevel(log.LogLevelWarning);
+//
+//	log.Warn("Hello");
+//	return 0;
+//}
+//
