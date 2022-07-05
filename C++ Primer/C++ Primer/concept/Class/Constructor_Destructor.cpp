840bd87f4636c178d9553fb68dbd45d20e02d474
云明正 <1374730170@qq.com>
2022-07-05 01:23:07 +0100

    Constructor

---
+++ C++ Primer/C++ Primer/concept/Class/Constructor_Destructor.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//
+//class CD {
+//public:
+//	int x, y;
+//
+//	CD() {
+//		x = 0;
+//		y = 1;
+//	}
+//	
+//	void Print() {
+//		std::cout << x << "," << y << std::endl;
+//	}
+//};
+//
+//int main() {
+//	CD cd;
+//	cd.Print();
+//}
