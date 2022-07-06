6e3e1cc985d9192a8a03ed5fc8c5e437f57b2435
云明正 <1374730170@qq.com>
2022-07-06 02:40:18 +0100

    const & mutable

---
+++ C++ Primer/C++ Primer/concept/KeyWords/Const_Mutable.cpp
@@ -0,0 +1,22 @@
+//#include<iostream>
+//
+//
+//class Entity {
+//private:
+//	std::string m_Name;
+//	mutable int n = 0;
+//public:
+//	const std::string& GetName() const
+//	{
+//		n++;
+//		return m_Name;
+//	}
+//};
+//
+//int main() {
+//	const Entity e;
+//	e.GetName();
+//
+//	std::cin.get();
+//	return 0;
+//}
