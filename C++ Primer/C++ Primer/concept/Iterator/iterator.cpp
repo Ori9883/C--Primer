e40c91d99b25a4c56f19ee9dd6bac66236f1bb48
云明正 <1374730170@qq.com>
2022-07-26 19:09:30 +0100

    review

--- C++ Primer/C++ Primer/concept/Iterator/iterator.cpp
+++ C++ Primer/C++ Primer/concept/Iterator/iterator.cpp
@@ -1,156 +1,156 @@
-////#include <iostream>
-////#include <string>
-////#include <vector>
+//////#include <iostream>
+//////#include <string>
+//////#include <vector>
+//////using namespace std;
+//////
+//////int main() {
+//////	string v = "name";
+//////	auto iterator = v.begin();
+//////	++iterator;
+//////	--iterator;
+//////
+//////}
+//////
+//////
+////// C++ program for the above approach
+////
+////#include <bits/stdc++.h>
 ////using namespace std;
 ////
-////int main() {
-////	string v = "name";
-////	auto iterator = v.begin();
-////	++iterator;
-////	--iterator;
+////// Function to find the largest possible
+////// subset having Bitwise AND positive
+////void largestSubset(int a[], int N)
+////{
+////	// Stores the number of set bits
+////	// at each bit position
+////	int bit[32] = { 0 };
 ////
+////	// Traverse the given array arr[]
+////	for (int i = 0; i < N; i++) {
+////
+////		// Current bit position
+////		int x = 31;
+////
+////		// Loop till array element
+////		// becomes zero
+////		while (a[i] > 0) {
+////
+////			// If the last bit is set
+////			if (a[i] & 1 == 1) {
+////
+////				// Increment frequency
+////				bit[x]++;
+////			}
+////
+////			// Divide array element by 2
+////			a[i] = a[i] >> 1;
+////
+////			// Decrease the bit position
+////			x--;
+////		}
+////	}
+////
+////	// Size of the largest possible subset
+////	cout << *max_element(bit, bit + 32);
 ////}
 ////
+////// Driver Code
+////int main()
+////{
+////	int arr[] = { 7, 13, 8, 2, 3 };
+////	int N = sizeof(arr) / sizeof(arr[0]);
+////	largestSubset(arr, N);
 ////
-//// C++ program for the above approach
+////	return 0;
+////}
+//// you can use includes, for example:
+//// #include <algorithm>
 //
-//#include <bits/stdc++.h>
-//using namespace std;
+//// you can write to stdout for debugging purposes, e.g.
+//// cout << "this is a debug message" << endl;
 //
-//// Function to find the largest possible
-//// subset having Bitwise AND positive
-//void largestSubset(int a[], int N)
-//{
-//	// Stores the number of set bits
-//	// at each bit position
-//	int bit[32] = { 0 };
+//#include <algorithm>
+//#include <string>
+//#include <vector>
 //
-//	// Traverse the given array arr[]
-//	for (int i = 0; i < N; i++) {
+//// int GetMaxDigit(int n);
+//// string ConvertToBitString(int num);
+//// string AndOperator(string& a, string& b);
+//int GetNum(int num);
 //
-//		// Current bit position
-//		int x = 31;
+//int solution(vector<int>& A) {
+//	// write your code in C++14 (g++ 6.2.0)
+//	//convert to binary
+//	// sort(A.begin(),A.end());
+//	// int max = A.back();
+//	// int length = GetMaxDigit(max);
 //
-//		// Loop till array element
-//		// becomes zero
-//		while (a[i] > 0) {
-//
-//			// If the last bit is set
-//			if (a[i] & 1 == 1) {
-//
-//				// Increment frequency
-//				bit[x]++;
+//	// vector<string> Astr;
+//	// for(auto i : A){
+//	//     string tempStr = ConvertToBitString(*i);
+//	//     Astr.emplace_back(tempStr);
+//	// }
+//	int n = A.length();
+//	int possibleTimes = 1 << n;
+//	int best = 0;
+//	for (int i = 0; i < possibleTimes; i++) {
+//		int temp = -1;
+//		for (int j = 0; j < n; j++) {
+//			if (possibleTimes & (1 << j) != 0)
+//			{
+//				temp &= nums[j];
 //			}
-//
-//			// Divide array element by 2
-//			a[i] = a[i] >> 1;
-//
-//			// Decrease the bit position
-//			x--;
+//		}
+//		if (temp != 0) {
+//			int num = GetNum(temp);
+//			if (num > best) {
+//				best = num;
+//			}
 //		}
 //	}
-//
-//	// Size of the largest possible subset
-//	cout << *max_element(bit, bit + 32);
+//	string a.append()
+//	return best;
 //}
 //
-//// Driver Code
-//int main()
-//{
-//	int arr[] = { 7, 13, 8, 2, 3 };
-//	int N = sizeof(arr) / sizeof(arr[0]);
-//	largestSubset(arr, N);
+//// int GetMaxDigit(int n){
+////     int temp = n;
+////     string bitString = NULL;
+////     while(n != 0){
+////         temp = n % 2;
+////         n >> 1;
+////         bitString.append('temp');
+////     }
+////     int length = bitString.size();
+////     return length;
+//// }
 //
-//	return 0;
+//int GetNum(int n) {
+//	int temp = n;
+//	string bitString = NULL;
+//	while (n != 0) {
+//		temp = n % 2;
+//		n >> 1;
+//		bitString.append(temp);
+//	}
+//	int num = 0;
+//	for (auto i = bitString.begin; i != bitString.end(); i++) {
+//		if (*i == 1)
+//			num++;
+//		else
+//			continue;;
+//	}
+//	return num;
 //}
-// you can use includes, for example:
-// #include <algorithm>
-
-// you can write to stdout for debugging purposes, e.g.
-// cout << "this is a debug message" << endl;
-
-#include <algorithm>
-#include <string>
-#include <vector>
-
-// int GetMaxDigit(int n);
-// string ConvertToBitString(int num);
-// string AndOperator(string& a, string& b);
-int GetNum(int num);
-
-int solution(vector<int>& A) {
-	// write your code in C++14 (g++ 6.2.0)
-	//convert to binary
-	// sort(A.begin(),A.end());
-	// int max = A.back();
-	// int length = GetMaxDigit(max);
-
-	// vector<string> Astr;
-	// for(auto i : A){
-	//     string tempStr = ConvertToBitString(*i);
-	//     Astr.emplace_back(tempStr);
-	// }
-	int n = A.length();
-	int possibleTimes = 1 << n;
-	int best = 0;
-	for (int i = 0; i < possibleTimes; i++) {
-		int temp = -1;
-		for (int j = 0; j < n; j++) {
-			if (possibleTimes & (1 << j) != 0)
-			{
-				temp &= nums[j];
-			}
-		}
-		if (temp != 0) {
-			int num = GetNum(temp);
-			if (num > best) {
-				best = num;
-			}
-		}
-	}
-	string a.append()
-	return best;
-}
-
-// int GetMaxDigit(int n){
-//     int temp = n;
-//     string bitString = NULL;
-//     while(n != 0){
-//         temp = n % 2;
-//         n >> 1;
-//         bitString.append('temp');
-//     }
-//     int length = bitString.size();
-//     return length;
-// }
-
-int GetNum(int n) {
-	int temp = n;
-	string bitString = NULL;
-	while (n != 0) {
-		temp = n % 2;
-		n >> 1;
-		bitString.append(temp);
-	}
-	int num = 0;
-	for (auto i = bitString.begin; i != bitString.end(); i++) {
-		if (*i == 1)
-			num++;
-		else
-			continue;;
-	}
-	return num;
-}
-
-// string AndOperator(string& a, string& b){
-//     string result = NULL;
-//     for(auto i = a.begin, j = b.begin; i < a.size(); i++,j++){
-//         if(*i == 1 && *j == 1){
-//             result.append('1');
-//         }
-//         else {
-//             result.append('0');
-//         }
-//     }
-//     return result;
-// }
+//
+//// string AndOperator(string& a, string& b){
+////     string result = NULL;
+////     for(auto i = a.begin, j = b.begin; i < a.size(); i++,j++){
+////         if(*i == 1 && *j == 1){
+////             result.append('1');
+////         }
+////         else {
+////             result.append('0');
+////         }
+////     }
+////     return result;
+//// }
